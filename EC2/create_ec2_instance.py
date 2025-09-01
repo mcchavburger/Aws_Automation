d2083e132500c056f49e2af3ce299bae834fa4e7
Dan Johnson <info@dojohnson.co.uk>
2025-09-01 20:52:40 +0100

    adding ec2

---
+++ EC2/create_ec2_instance.py
@@ -0,0 +1,12 @@
+'''
+These scripts have been written in order to test and further my python and boto3 understanding, they are not intend (at this point) to be used in any production environment
+However please feel free to use these scripts any way you wish, dont blame me if stuff breaks :-)
+
+'''
+
+import boto3
+
+client = boto3.client('ec2')
+
+
+
